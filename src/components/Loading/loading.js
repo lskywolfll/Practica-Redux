d9b053bd37827b03b05f96781726c295ba026b10
lskywolfll <renealejandrosanchezmorales@gmail.com>
2020-08-01 18:25:39 -0400

    se agrega componente del loading para cuando se seta cargano informacion en la api

--- src/components/Loading/loading.js
+++ src/components/Loading/loading.js
@@ -4,15 +4,17 @@ import './styles/loading.css';
 export default class loading extends Component {
     render() {
         return (
-            <div class="lds-roller">
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
+            <div>
+                <div class="lds-roller">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
             </div>
         )
     }
